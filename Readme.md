b21ed3da86bd0e48d3f9fa91e945bdadd7e998b8
Atharva Kumar Suman <atharvasuman16@gmail.com>
2025-07-20 23:17:45 +0530

    Added files

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# ⏰ Alarm Clock (Terminal-Based)
+
+A simple alarm clock application built using Python. It allows the user to set a specific time and plays a sound when the time is reached.
+
+## 📦 Features
+
+- Takes user input in HH:MM:SS format
+- Plays an audio file when the alarm rings
+- Displays the current time every second in the terminal
---
+++ alarm_clock.py
@@ -0,0 +1,33 @@
+import datetime
+import time
+import pygame
+
+def set_alarm(alarm_time):
+    print(f"Alarm set for {alarm_time}")
+
+    sound_file = "assets/my_music.mp3"
+    pygame.mixer.init()
+    pygame.mixer.music.load(sound_file)
+
+    is_running = True
+    
+    while is_running:
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        print("Current Time: " + current_time, end='\r')
+
+        if current_time == alarm_time:
+            print("⏰ Wake Up!")
+
+            pygame.mixer.music.play()
+            time.sleep(30)
+            pygame.mixer.music.stop()
+
+            is_running=False
+
+        time.sleep(1)
+
+
+if __name__=="__main__":
+    print("===== Alarm Clock (Terminal-Based) =====")
+    alarm_time = input("Enter the alarm time (HH:MM:SS): ")
+    set_alarm(alarm_time)
